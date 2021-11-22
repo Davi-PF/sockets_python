e1bb2dba1856db3d2cc06de6ef486a01c4cac415
Davi Prudente Ferreira <daviprufer@gmail.com>
2021-11-22 23:26:05 +0000

    teste

---
+++ echo-server.py
@@ -0,0 +1,16 @@
+import socket
+
+server = '127.0.0.1' # localhost - loopback
+port = 12345
+
+with socket.socket(socket.AF_INET, socket.SOC_STREAM) as sock:
+    sock.bind((server, port))
+    sock.listen()
+    conn, addr = sock.accept()          # Informação do cliente que conectou
+    with conn:                          # Trabalhando com a conexão do cliente
+        print(f'Cliente conectou com o endereço: {addr}')
+        while True:                     # Loop infinito
+            data = conn.recv(1024)      # Leia 1024 bytes
+            if not data:                # Se não leu nada
+                break                   # Sai do while
+            conn.sendall(data)          # Se leu alguma coisa -> devolve pro cliente
