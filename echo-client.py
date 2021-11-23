80947671cd37738fb1b3a580b869a072848bfd7c
Davi Prudente Ferreira <94153717+Deividiii@users.noreply.github.com>
2021-11-22 21:05:39 -0300

    Create echo-client.py

---
+++ echo-client.py
@@ -0,0 +1,11 @@
+import socket
+
+server = '127.0.0.1'   # endereço do servidor ao qual vamos se conectar
+port = 12345           # porta que vamos conectar
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:  # Abre o socket TCP
+    socket.connect((server, port))
+    sock.sendall(b'Hello, World!')
+    data = sock.recv(1024)
+
+print('Recebido ', repr(data))
